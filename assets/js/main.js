a7e02d0ab7c0b24f27a7551469b95872cb9e21df
Ravi Teja <geddadaraviteja612@gmail.com>
2024-01-18 07:55:21 -0500

    full screen certificate images

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,18 +45,18 @@
   document.addEventListener('contextmenu', (e) => e.preventDefault());
 
   function ctrlShiftKey(e, keyCode) {
-    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
   }
 
   document.onkeydown = (e) => {
-    // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-    if (
-      event.keyCode === 123 ||
-      ctrlShiftKey(e, 'I') ||
-      ctrlShiftKey(e, 'J') ||
-      ctrlShiftKey(e, 'C') ||
-      (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-    )
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+  event.keyCode === 123 ||
+  ctrlShiftKey(e, 'I') ||
+  ctrlShiftKey(e, 'J') ||
+  ctrlShiftKey(e, 'C') ||
+  (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
       return false;
   };
 
@@ -213,6 +213,15 @@
    */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Initiate portfolio details lightbox 
+   */
+  const certificateDetailsLightbox = GLightbox({
+    selector: '.certificates-lightbox',
+    width: '90%',
+    height: '90vh'
   });
 
   /**
